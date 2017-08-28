6eb9dc076b3e7dab0aab48cb69b55b72fe58a735
zhixinli0904 <30875204+zhixinli0904@users.noreply.github.com>
2017-08-28 08:48:37 -1000

    Add files via upload

---
+++ object literal.js
@@ -0,0 +1,10 @@
+var x = prompt("enter firstName, lastName, age, or grade")
+
+var students= {
+	fname:"zhixin",
+  lname:"li",
+  age:16,
+  grade:11
+};
+window.alert(students.fname=x);
+window.alert(students.fname);
